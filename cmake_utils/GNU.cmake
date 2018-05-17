5a61b3f5a2087a96a26053b7758442fe8e111db5
Tom Clune <tom.clune@nasa.gov>
2018-05-17 12:32:17 -0400

    Added missing compiler definition files.

---
+++ cmake_utils/GNU.cmake
@@ -0,0 +1,11 @@
+# Compiler specific flags for Intel Fortran compiler
+
+set(no_optimize "-O0")
+set(check_all "-fcheck=bounds -fcheck=pointer -fcheck=mem ")
+set(traceback "-fbacktrace")
+set(cpp "-cpp")
+
+
+set(CMAKE_Fortran_FLAGS_DEBUG  "${no_optimize}")
+set(CMAKE_Fortran_FLAGS_RELEASE "-O3")
+set(CMAKE_Fortran_FLAGS "-g ${cpp} ${traceback} ${check_all}")
--- include/types/Foo.m4
+++ include/types/Foo.m4
@@ -1,6 +1,6 @@
 include(header.m4)
 
-      use Foo_mod
+      use Foo_mod, only: Foo
 #define _param() type(Foo)
 #define _base()_equal_defined
 #define _BASE()_LESS_THAN(x,y) (x%i < y%i)
