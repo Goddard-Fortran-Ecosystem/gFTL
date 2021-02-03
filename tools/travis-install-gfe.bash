9401071ce1e6d3ed58c2cb7aac7f3679bbf2a899
Matthew Thompson <fortran@gmail.com>
2021-02-03 12:52:14 -0500

    Remove unneeded comment

--- tools/travis-install-gfe.bash
+++ tools/travis-install-gfe.bash
@@ -15,7 +15,6 @@ do
    cd ${GFE_DIR}
    git clone https://github.com/Goddard-Fortran-Ecosystem/${repo}.git
    cd ${GFE_DIR}/${repo}
-   # Necessary for consistent CMake targets
    mkdir build && cd build
    cmake .. -DCMAKE_INSTALL_PREFIX=${GFE_INSTALL_DIR} -DCMAKE_PREFIX_PATH=${GFE_INSTALL_DIR}
    make -j$(nproc) install
