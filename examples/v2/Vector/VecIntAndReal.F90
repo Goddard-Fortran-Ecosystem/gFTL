8b56c021c0966df89f09832458e0fb058d1b8565
Emily Bourne <emily.bourne@epfl.ch>
2024-10-03 10:37:03 +0200

    Expand examples

--- examples/v2/Vector/VecIntAndReal.F90
+++ examples/v2/Vector/VecIntAndReal.F90
@@ -42,30 +42,45 @@ program main
    implicit none
    type (VectorInt) :: iv
    type (VectorReal64) :: rv
+   type (VectorInt) :: iv_range
+   type (VectorIntIterator) :: iv_iter
    real(real64) :: r
 
    iv=VectorInt()
    rv=VectorReal64()
+   iv_range=VectorInt([1,2,3,4,5,6,7,8,9,10])
 
    r = 1.0d0
    call iv%push_back(1)
    call rv%push_back(r)
-   call check(iv,1)
-   call checkr(rv,r)
+   call iv_range%push_back(20)
+   call check(iv,1,1)
+   call checkr(rv,1,r)
+   print *, "iv_range%size() = ", iv_range%size()
+   call check(iv_range, 1, 1)
+   call check(iv_range, 10, 10)
+   call check(iv_range, 11, 20)
+   iv_iter = iv_range%begin() + 5
+   iv_iter = iv_range%insert(iv_iter, 30)
+   call check(iv_range, 6, 30)
+   call check(iv_range, 7, 6)
+   call check(iv_range, 12, 20)
 contains
 
-   subroutine check(iv, expected)
+   subroutine check(iv, idx, expected)
       type(VectorInt), intent(in) :: iv
+      integer, intent(in) :: idx
       integer, intent(in) :: expected
 
-      print*,"iv%at('",1,"')  = ",iv%at(1),"(should be",expected,")"
+      print*,"iv%at('",idx,"')  = ",iv%at(idx),"(should be",expected,")"
    end subroutine check
 
-   subroutine checkr(rv, expected)
+   subroutine checkr(rv, idx, expected)
       type(VectorReal64), intent(in) :: rv
+      integer, intent(in) :: idx
       real(real64), intent(in) :: expected
 
-      print*,"rv%at('",1,"')  = ",rv%at(1),"(should be",expected,")"
+      print*,"rv%at('",idx,"')  = ",rv%at(idx),"(should be",expected,")"
    end subroutine checkr
 end program main
 
