8c64cdfc3710f3c2c30982ffc34e92e7ab6826f6
Matthew Thompson <matthew.thompson@nasa.gov>
2025-03-06 15:38:34 -0500

    Set minimum CMake

--- cmake/Lfortran.cmake
+++ cmake/Lfortran.cmake
@@ -1,5 +1,8 @@
 # Compiler specific flags for LFortran compiler
 
+# LFortran support came in CMake 3.31
+cmake_minimum_required(VERSION 3.31)
+
 set(cpp "--cpp")
 
 set(CMAKE_Fortran_FLAGS_DEBUG  "-g -O0 ${cpp}")
