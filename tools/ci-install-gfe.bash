a27323d551ae4bbc50cbd6919407d9a4adb9ea75
Matthew Thompson <matthew.thompson@nasa.gov>
2026-09-11 11:16:51 -0400

    feat(ci): add Windows CI workflow

--- tools/ci-install-gfe.bash
+++ tools/ci-install-gfe.bash
@@ -2,21 +2,25 @@
 
 set -e
 
-GFE_DIR=${HOME}/gfe
-mkdir -p ${GFE_DIR}
+GFE_DIR=${GFE_DIR:-${HOME}/gfe}
+mkdir -p "${GFE_DIR}"
 
 # First install prerequisites
-GFE_INSTALL_DIR=${HOME}/Software/GFE
-mkdir -p ${GFE_INSTALL_DIR}
+GFE_INSTALL_DIR=${GFE_INSTALL_DIR:-${HOME}/Software/GFE}
+mkdir -p "${GFE_INSTALL_DIR}"
 
 to_build=(pFUnit)
 for repo in "${to_build[@]}"
 do
-   cd ${GFE_DIR}
+   cd "${GFE_DIR}"
    git clone https://github.com/Goddard-Fortran-Ecosystem/${repo}.git
-   cd ${GFE_DIR}/${repo}
-   mkdir build && cd build
-   cmake .. -DCMAKE_INSTALL_PREFIX=${GFE_INSTALL_DIR} -DCMAKE_PREFIX_PATH=${GFE_INSTALL_DIR}
-   make -j4 install
+   cd "${GFE_DIR}/${repo}"
+   cmake -B build \
+      -DCMAKE_INSTALL_PREFIX="${GFE_INSTALL_DIR}" \
+      -DCMAKE_PREFIX_PATH="${GFE_INSTALL_DIR}" \
+      -DENABLE_TESTS=OFF \
+      "$@"
+   cmake --build build --parallel 4 --target install
 done
+
 
